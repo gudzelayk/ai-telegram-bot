123450ec25759b57f069bc2bbaaf5c6d0592a54d
gudzelayk <35800954+gudzelayk@users.noreply.github.com>
2025-05-21 23:08:43 +0300

    Update agent.py

--- agent.py
+++ agent.py
@@ -3,7 +3,7 @@ import os
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-model = genai.GenerativeModel("gemini-pro")
+model = genai.GenerativeModel("gemini-2.0-flash")
 
 def ask_gpt(prompt):
     try:
